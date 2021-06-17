cbaf82b20d3416be0206463b50e232bb3a6bb2b0
Hamed Aarab <hawmed.a@gmail.com>
2021-06-17 11:03:06 +0430

    removed jsdoc and test script

--- src/nexbounce.js
+++ src/nexbounce.js
@@ -1,21 +1,5 @@
 'use strict';
 
-/**
- * @classdesc Batches calls and calls the last task in its queue.
- * @example
- * import { Nexbounce } from 'nexbounce';
- *
- * const nexbounce = new Nexbounce();
- *
- * let counter = 0;
- *
- * nexbounce.enqueue(() => (counter += 3));
- * nexbounce.enqueue(() => (counter += 1));
- * nexbounce.enqueue(() => (counter += 2));
- *
- * setTimeout(() => console.log(counter)); // 2
- */
-
 export class Nexbounce {
   #taskWatcher = null;
   #callLatest;
